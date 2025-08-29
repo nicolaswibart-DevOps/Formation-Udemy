03be1e68ef68797b4a6f0dcb13963cf8f8e58a4b
nicolaswibart-DevOps <nicolas.wibart@gmail.com>
2025-08-29 17:27:32 +0200

    Add files via upload

---
+++ Calculatrice.py
@@ -0,0 +1,14 @@
+#Conception d'une calculatrice
+
+#Demander à l'utilisateur un premier nombre
+#Demander à l'utilisateur un deuxième nombre
+#Afficher le message "Le résultat de l'addition du nombre a avec le nombre b est égal à a+b"
+
+
+a = input("veuiller entrer un premier nombre:")
+b = input("veuiller entrer un deuxieme nombre:")
+
+
+resultat = f"Le résultat de l'addition du nombre {a} avec le nombre {b} est égal à {int(a) + int(b)}"
+
+print(resultat)

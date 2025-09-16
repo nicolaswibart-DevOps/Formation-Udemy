394d07f4c9f2617aa057044b9ddde39af00b3325
nicolaswibart-DevOps <nicolas.wibart@gmail.com>
2025-09-16 18:48:11 +0200

    liste_de_courses.py (avec sauvegarde)

--- liste_de_courses.py
+++ liste_de_courses.py
@@ -1,4 +1,11 @@
 import sys
+import json
+import os
+
+CUR_DIR = os.path.dirname(__file__)
+chemin = os.path.join(CUR_DIR, "liste_de_course.json")
+
+
 # Construire les choix et l'afficher
 
 Actions = """Choisissez parmi les 5 options suivantes :
@@ -17,7 +24,11 @@ choix_utilisateur = ""
 
 MENU_CHOICES = ["1", "2", "3", "4", "5"]
 
-Liste_de_course = []
+if os.path.exists(chemin):
+    with open (chemin, "r" ) as f:
+        LISTE = json.load(f)
+else:
+    LISTE = []
 
 while True:  # boucle principale du menu
     print(Actions)
@@ -30,35 +41,38 @@ while True:  # boucle principale du menu
 
     if choix_utilisateur == "1": #Action 1
         ajout_element = input("Entrer le nom de l'élément à ajouter à la liste: ")
-        Liste_de_course.append(ajout_element)
+        LISTE.append(ajout_element)
         print(f"✅ L'élément {ajout_element} a bien été ajouté à votre liste.")
 
     elif choix_utilisateur == "2": #Action 2
         supprimer_element = input ("Entrer le nom d'un élément à retirer de la liste: ")
-        if supprimer_element in Liste_de_course:
-            Liste_de_course.remove(supprimer_element)
+        if supprimer_element in LISTE:
+            LISTE.remove(supprimer_element)
             print(f"🗑️ L'élément {supprimer_element} a bien été retiré de votre liste.")
         else:
             print(f"⚠️ L'élément {supprimer_element} n'est pas dans votre liste.")
 
     elif choix_utilisateur == "3": #Action 3
-        if Liste_de_course: #Test si liste vide
+        if LISTE: #Test si liste vide
             print("🛒 Votre liste de course : ")
-            for i, element in enumerate(Liste_de_course, 1):
+            for i, element in enumerate(LISTE,1):
                 print(f"{i}. {element}")
         else:
             print("⚠️ Votre liste est vide.")
 
     elif choix_utilisateur == "4": #Action 4
-        if Liste_de_course:
-            Liste_de_course.clear()
+        if LISTE:
+            LISTE.clear()
             print("✅ Votre liste a été vidée")
         else:
             print("⚠️ Votre liste est déjà vide.")
 
-    elif choix_utilisateur == "5": #Action 5
+    elif choix_utilisateur == "5":
+        with open (chemin, "w" ) as f:
+            json.dump(LISTE, f, indent=4) #Action 5
         print("À bientôt !")
         sys.exit()
+    
 
     print("-" * 50)
 
